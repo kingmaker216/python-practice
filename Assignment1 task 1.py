8d14747d65b30312c731996a0a37864513fe7059
kingmaker216 <gorakhnathgodse@gmail.com>
2025-05-03 23:38:13 +0530

    Add files via upload

---
+++ Assignment1 task 1.py
@@ -0,0 +1,14 @@
+a=input('enter the first number: ')
+b=input('enter the second number: ')
+a=int(a)
+b=int(b)
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print(' Addition =',add)
+print(' Subtraction =',sub)
+print(' Multiplication =',mul)
+print(' Division =',div)
---
+++ assisgnment 2 task.py
@@ -0,0 +1,8 @@
+import string
+
+a=input('enter your first name: ')
+b=input('enter your last name: ')
+a=str(a)
+b=str(b)
+c= a+ ' '+ b
+print("hello, ",c,"! welcome to the python program")
